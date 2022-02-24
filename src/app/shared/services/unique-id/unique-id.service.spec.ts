8cf98114364f83a830cd802dc6dd13977e070a93
Joao Pedro Bruno Machado <joao.machado0601@gmail.com>
2022-02-24 12:09:52 -0300

    Adding new tests

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -1,9 +1,27 @@
 import { UniqueServiceId as UniqueIdService } from './unique-id.service'
 
+const service = new UniqueIdService
+
 describe(UniqueIdService.name, () => {
   it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix} should generate id when called with prefix`, () => {
-    const service = new UniqueIdService
     const id = service.generateUniqueIdWithPrefix('app')
-    expect(id).toContain('app-')
+    expect(id.startsWith('app-')).toBeTrue()
+  })
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix} should not generate duplicate IDs when called multiple times`, () => {
+    const ids = new Set()
+
+    for(let i = 0; i < 50; i++){
+      ids.add(service.generateUniqueIdWithPrefix('app'))
+    }
+
+    expect(ids.size).toBe(50)
+  })
+
+  it(`#${UniqueIdService.prototype.getNumberOfGeneratedUniqueIds.name} should return the number of generated ids when called`, () => {
+    service.generateUniqueIdWithPrefix('app')
+    service.generateUniqueIdWithPrefix('app')
+
+    expect(service.getNumberOfGeneratedUniqueIds()).toBe(2)
   })
 })
