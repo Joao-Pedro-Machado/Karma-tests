e5b63cd6a6b5af6dc2c3366bda7fa22b3dfb9db8
Joao Pedro Bruno Machado <joao.machado0601@gmail.com>
2022-02-24 14:39:14 -0300

    Refactoring tests

--- src/app/shared/services/unique-id/unique-id.service.ts
+++ src/app/shared/services/unique-id/unique-id.service.ts
@@ -2,24 +2,26 @@ import { Injectable } from '@angular/core';
 import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
-export class UniqueServiceId {
+export class UniqueIdService {
+
   private numberOfGeneratedIds = 0;
 
-  public generateUniqueIdWithPrefix(prefix: string): string {
-    if(!prefix) {
-      throw Error('Prefix can not be empty')
-    }
+  private validId = /^[A-Za-z]+[\w\-\:\.]*$/;
 
-    const uniqueId = this.generateUniqueId()
-    this.numberOfGeneratedIds++
-    return `${prefix}-${uniqueId}`
+  public generateUniqueIdWithPrefix(prefix: string): string {
+    if (!prefix || !this.validId.test(prefix)) {
+      throw Error('Prefix can not be empty');
+    }
+    const uniqueId = this.generateUniqueId();
+    this.numberOfGeneratedIds++;
+    return `${prefix}-${uniqueId}`;
   }
 
   public getNumberOfGeneratedUniqueIds(): number {
-    return this.numberOfGeneratedIds
+    return this.numberOfGeneratedIds;
   }
 
   private generateUniqueId(): string {
-    return uuidv4()
+    return uuidv4();
   }
 }
